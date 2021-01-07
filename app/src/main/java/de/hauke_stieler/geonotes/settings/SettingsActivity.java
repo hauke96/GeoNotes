1e3e63c439a748940a3226c036f1b87750f1192c
Hauke Stieler <mail@hauke-stieler.de>
2021-01-07 20:53:49 +0100

    Store settings on back-button click

--- app/src/main/java/de/hauke_stieler/geonotes/settings/SettingsActivity.java
+++ app/src/main/java/de/hauke_stieler/geonotes/settings/SettingsActivity.java
@@ -1,6 +1,9 @@
 package de.hauke_stieler.geonotes.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,18 +19,37 @@ public class SettingsActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setNavigationOnClickListener(v -> save());
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        load();
+    }
+
+    private void load() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+
+        boolean prefZoomButtons = preferences.getBoolean(getString(R.string.pref_zoom_buttons), true);
+        ((Switch) findViewById(R.id.settings_zoom_switch)).setChecked(prefZoomButtons);
+
+        float prefMapScaling = preferences.getFloat(getString(R.string.pref_map_scaling), 1.0f);
+        ((EditText) findViewById(R.id.settings_scale_input)).setText("" + prefMapScaling);
     }
 
     private void save() {
-        // TODO
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        boolean checked = ((Switch) findViewById(R.id.settings_zoom_switch)).isChecked();
+        editor.putBoolean(getString(R.string.pref_zoom_buttons), checked);
+        String s = ((EditText) findViewById(R.id.settings_scale_input)).getText().toString();
+        editor.putFloat(getString(R.string.pref_map_scaling), Float.parseFloat(s));
+        editor.commit();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
+        save();
         finish();
         return true;
     }
