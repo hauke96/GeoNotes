b3d3fb3228ce3a51bc245fc37214acfbbea659c8
Hauke Stieler <mail@hauke-stieler.de>
2021-01-11 01:43:27 +0100

    Remove unnecessary loading of preferences

--- app/src/main/java/de/hauke_stieler/geonotes/MainActivity.java
+++ app/src/main/java/de/hauke_stieler/geonotes/MainActivity.java
@@ -130,7 +130,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        loadPreferences();
         map.onResume();
     }
 
