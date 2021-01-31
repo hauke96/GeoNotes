7de1372d9eb5ae7fa46b1ee0142432f70661c760
Hauke Stieler <mail@hauke-stieler.de>
2021-01-31 22:35:11 +0100

    Implement PhotoStore

--- app/src/main/java/de/hauke_stieler/geonotes/photo/PhotoStore.java
+++ app/src/main/java/de/hauke_stieler/geonotes/photo/PhotoStore.java
@@ -1,23 +1,52 @@
 package de.hauke_stieler.geonotes.photo;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hauke_stieler.geonotes.notes.Note;
 
 public class PhotoStore {
     private static final String PHOTOS_TABLE_NAME = "photos";
     private static final String PHOTOS_COL_ID = "id";
     private static final String PHOTOS_COL_NOTE_ID = "note";
-    private static final String PHOTOS_COL_PATH = "path";
+    private static final String PHOTOS_COL_FILE_NAME = "path";
 
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(String.format("CREATE TABLE IF NOT EXISTS %s(%s INTEGER PRIMARY KEY, %s INTEGER NOT NULL, %s VARCHAR NOT NULL);",
                 PHOTOS_TABLE_NAME,
                 PHOTOS_COL_ID,
                 PHOTOS_COL_NOTE_ID,
-                PHOTOS_COL_PATH));
+                PHOTOS_COL_FILE_NAME));
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.i("NoteStore", String.format("onUpgrade: from version %d to version %d", oldVersion, newVersion));
+    }
+
+    public void addPhoto(SQLiteDatabase db, Note note, File photoFile) {
+        ContentValues values = new ContentValues();
+        values.put(PHOTOS_COL_NOTE_ID, note.id);
+        values.put(PHOTOS_COL_FILE_NAME, photoFile.getName());
+
+        db.insert(PHOTOS_TABLE_NAME, null, values);
+    }
+
+    public List<String> getPhotos(SQLiteDatabase db, Note note) {
+        Cursor cursor = db.query(PHOTOS_TABLE_NAME, new String[]{PHOTOS_COL_NOTE_ID, PHOTOS_COL_FILE_NAME}, PHOTOS_COL_NOTE_ID, new String[]{note.id + ""}, null, null, null);
+
+        List<String> photos = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            do {
+                photos.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+
+        return photos;
     }
 }
