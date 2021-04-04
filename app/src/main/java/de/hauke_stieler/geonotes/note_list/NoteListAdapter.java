042c02096e316b5e933dad93edeee26d13cc3f60
Hauke Stieler <mail@hauke-stieler.de>
2021-04-04 21:35:53 +0200

    Write, fix and adjust tests

--- app/src/main/java/de/hauke_stieler/geonotes/note_list/NoteListAdapter.java
+++ app/src/main/java/de/hauke_stieler/geonotes/note_list/NoteListAdapter.java
@@ -19,12 +19,14 @@ public class NoteListAdapter extends BaseAdapter {
         void onClick(long id);
     }
 
+    private final Context context;
     private final List<Note> notes;
     private final List<Note> notesWithPhoto;
     private final NoteListClickListener clickListener;
     private final LayoutInflater inflater;
 
     public NoteListAdapter(Context context, List<Note> notes, List<Note> notesWithPhoto, NoteListClickListener clickListener) {
+        this.context = context;
         this.notes = notes;
         this.notesWithPhoto = notesWithPhoto;
         this.clickListener = clickListener;
@@ -59,22 +61,31 @@ public class NoteListAdapter extends BaseAdapter {
         boolean noteHasPhotos = notesWithPhoto.contains(note);
 
         ImageView icon = view.findViewById(R.id.note_list_row_icon);
+        fillIconView(noteHasPhotos, icon);
+
+        TextView text = view.findViewById(R.id.note_list_row_text_view);
+        fillTextView(note, noteHasPhotos, text);
+
+        return view;
+    }
+
+    void fillTextView(Note note, boolean noteHasPhotos, TextView text) {
+        text.setOnClickListener(v -> this.clickListener.onClick(note.getId()));
+
+        if (noteHasPhotos && note.getDescription().trim().isEmpty()) {
+            text.setText("(only photo)");
+            text.setTypeface(null, Typeface.ITALIC);
+            text.setTextColor(context.getResources().getColor(R.color.grey));
+        } else {
+            text.setText(note.getDescription());
+        }
+    }
+
+    void fillIconView(boolean noteHasPhotos, ImageView icon) {
         if (noteHasPhotos) {
             icon.setImageResource(R.mipmap.ic_note_photo);
         } else {
             icon.setImageResource(R.mipmap.ic_note);
         }
-
-        TextView text = view.findViewById(R.id.note_list_row_text_view);
-        text.setOnClickListener(v -> this.clickListener.onClick(note.getId()));
-        if(noteHasPhotos && note.getDescription().trim().isEmpty()) {
-            text.setText("(only photo)");
-            text.setTypeface(null, Typeface.ITALIC);
-            text.setTextColor(text.getResources().getColor(R.color.grey));
-        }else{
-            text.setText(note.getDescription());
-        }
-
-        return view;
     }
 }
