9e2a5e771a1a212fe5f50e7f9b0dc3ae4f18f36b
Hauke Stieler <mail@hauke-stieler.de>
2021-08-17 23:09:06 +0200

    Use application/geo+json as MIME type for exports

--- app/src/main/java/de/hauke_stieler/geonotes/export/Exporter.java
+++ app/src/main/java/de/hauke_stieler/geonotes/export/Exporter.java
@@ -6,8 +6,9 @@ import android.util.Log;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.hauke_stieler.geonotes.common.FileHelper;
 import de.hauke_stieler.geonotes.database.Database;
@@ -16,7 +17,7 @@ public class Exporter {
     private final Database database;
     private final Context context;
 
-    public Exporter(Database database, Context context){
+    public Exporter(Database database, Context context) {
         this.database = database;
         this.context = context;
     }
@@ -25,8 +26,10 @@ public class Exporter {
         String geoJson = GeoJson.toGeoJson(database.getAllNotes());
 
         try {
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+
             File storageDir = context.getExternalFilesDir("GeoNotes");
-            File exportFile = new File(storageDir, "geonotes-export.geojson");
+            File exportFile = new File(storageDir, "geonotes-export_" + timeStamp + ".geojson");
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(exportFile));
             outputStreamWriter.write(geoJson);
             outputStreamWriter.close();
@@ -34,12 +37,12 @@ public class Exporter {
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
             sendIntent.putExtra(Intent.EXTRA_STREAM, FileHelper.getFileUri(context, exportFile));
-            sendIntent.setType("application/json");
+            sendIntent.setType("application/geo+json");
 
             Intent shareIntent = Intent.createChooser(sendIntent, null);
             shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // needed because we're outside of an activity
             context.startActivity(shareIntent);
-        } catch (IOException e) {
+        } catch (Exception e) {
             Log.e("Export", "File write failed: " + e.toString());
         }
     }
